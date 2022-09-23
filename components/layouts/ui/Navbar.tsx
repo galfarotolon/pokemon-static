7ddeb8c5f75040114829d54427da724a14271850
Guillermo Alfaro <galfarotolon@gmail.com>
2022-09-23 12:57:38 -0500

    section 4 finish

--- components/layouts/ui/Navbar.tsx
+++ components/layouts/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, Spacer, useTheme } from "@nextui-org/react";
+import { Text, Spacer, useTheme, Link } from "@nextui-org/react";
 import Image from "next/image";
+import NextLink from "next/link";
 
 export const Navbar = () => {
   const { theme } = useTheme();
@@ -25,16 +26,24 @@ export const Navbar = () => {
         height={70}
       />
 
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okemon
-      </Text>
+      <NextLink href={"/"} passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okemon
+          </Text>
+        </Link>
+      </NextLink>
 
       <Spacer css={{ flex: 1 }} />
 
-      <Text color="white">Favoritos</Text>
+      <NextLink href={"/favorites"} passHref>
+        <Link>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { Layout } from "../../components/layouts/Layout";
+
+const FavoritesPage: NextPage = () => {
+  return <Layout title="favorites">favorites</Layout>;
+};
+
+export default FavoritesPage;
