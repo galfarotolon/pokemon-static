5afe8b0c7ea0333347c198c287162c6e21a0e666
Guillermo Alfaro <galfarotolon@gmail.com>
2022-09-28 19:08:39 -0500

    fix btn for mobile view

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -69,7 +69,11 @@ const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
         <Grid xs={12} sm={8}>
           <Card>
             <Card.Header
-              css={{ display: "flex", justifyContent: "space-between" }}
+              css={{
+                display: "flex",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+              }}
             >
               <Text h1 transform="capitalize">
                 {pokemon.name}
